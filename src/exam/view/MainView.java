e014994bb3b40e2dfb3428bc6cdb74e13991a9f4
supreme4rest <supreme4rest@gmail.com>
2025-08-28 14:30:57 +0900

    main modifier

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -3,8 +3,8 @@ package exam.view;
 public class MainView {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("시작합니다.~~^^");
+		
 	}
 
 }
---
+++ src/exam/view/MenuView.java
@@ -0,0 +1,9 @@
+package exam.view;
+
+public class MenuView {
+	public static void main(String[] args) {
+		System.out.println("시작합니다");
+		System.out.println("배고파요");
+		System.out.println("main 수정");
+	}
+}
